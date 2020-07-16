456c30b8dbc2eb8d0c8ecee8fcbda61febc0fc91
keroro <keroroxx520@gmail.com>
2020-07-16 18:03:32 +0800

    test(sync): add topic cases

--- src/sync/tests/topic.rs
+++ src/sync/tests/topic.rs
@@ -20,6 +20,44 @@ fn test_subscribe_after_all_published() {
     consume_ordered(sub_response_receiver, &payloads);
 }
 
+#[test]
+fn test_subscribe_first_concurrent_writes() {
+    let iterations: u64 = 1000;
+    let (runenv, _output_dir) = random_test_run_env();
+    let mut client = Client::new(runenv).expect("new client");
+    let payloads = (0..iterations).collect::<Vec<_>>();
+    let topic = Topic::new("virus");
+
+    let sub_response_receiver = client.subscribe(topic.clone()).expect("subscribe");
+
+    let mut client_clone = client.clone();
+    let topic_clone = topic.clone();
+    let payloads_clone = payloads.clone();
+    spawn(move || {
+        produce(&mut client_clone, &topic_clone, &payloads_clone);
+    });
+
+    consume_ordered(sub_response_receiver, &payloads);
+}
+
+#[test]
+fn test_subscribe_duplicate_topic() {
+    let (runenv, _output_dir) = random_test_run_env();
+    let mut client = Client::new(runenv).expect("new client");
+    let topic = Topic::new("immune");
+
+    let _ = client.subscribe(topic.clone()).expect("subscribe");
+    let sub_response_receiver = client.subscribe(topic.clone()).expect("subscribe");
+
+    assert!(
+        sub_response_receiver
+            .recv_timeout(Duration::from_secs(1))
+            .unwrap()
+            .is_err(),
+        "duplicated subscription"
+    );
+}
+
 fn produce(client: &mut Client, topic: &Topic, payloads: &Vec<Payload>) {
     let rp = client.runenv.run_params();
     let redis_key = topic.redis_key(rp);
