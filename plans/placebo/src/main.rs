935182d3614662c9cc8d347f61e74e474977f31d
keroro <keroroxx520@gmail.com>
2020-07-17 13:14:59 +0800

    feat(plans/placebo): impl placebo code

--- plans/placebo/src/main.rs
+++ plans/placebo/src/main.rs
@@ -1,3 +1,20 @@
+use std::thread::sleep;
+use std::time::Duration;
+use testground::run::invoker::invoke;
+use testground::runtime::runenv::RunEnv;
+
 fn main() {
-    println!("Hello, world!");
+    invoke(run_);
+}
+
+fn run_(runenv: RunEnv) -> Result<(), String> {
+    match runenv.test_case.as_str() {
+        "ok" => return Ok(()),
+        "panic" => panic!("this is an intentional panic".to_string()),
+        "stall" => {
+            sleep(Duration::from_secs(24 * 60 * 60 /* 24h */));
+            return Ok(());
+        }
+        _ => return Err("aborting".to_string()),
+    }
 }
